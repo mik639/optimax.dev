3eb1f25c91c309e9abd38a812b34af541964b84e
Roman <ya-hostel@yandex.ru>
2019-06-20 18:32:59 +0400

    fixed scroll

--- src/components/home/firstScreen/firstScreen.tsx
+++ src/components/home/firstScreen/firstScreen.tsx
@@ -7,19 +7,21 @@ import s from './firstScreen.module.scss';
 class FirstScreen extends Component {
     render(): ReactNode {
         return (
-            <div className={s.wrap}>
-                <div className={s.content}>
-                    <Title>Work With Us</Title>
-                    <span className={s.desc}>
-                        We are a worldwide e-commerce
-                        <br />
-                        leader in the eyewear industry.
-                    </span>
-                    <a className={s.applyNow} href="mailto:example.com">
-                        Apply now
-                    </a>
+            <div className={s.container}>
+                <div className={s.wrap}>
+                    <div className={s.content}>
+                        <Title>Work With Us</Title>
+                        <span className={s.desc}>
+                            We are a worldwide e-commerce
+                            <br />
+                            leader in the eyewear industry.
+                        </span>
+                        <a className={s.applyNow} href="mailto:example.com">
+                            Apply now
+                        </a>
+                    </div>
+                    <div className={s.bg} />
                 </div>
-                <div className={s.bg} />
             </div>
         );
     }
