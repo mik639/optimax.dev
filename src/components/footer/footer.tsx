12852547fd848666e5e615cd2c7f5c1ba7066ab1
Roman <ya-hostel@yandex.ru>
2019-06-20 18:40:23 +0400

    fixed footer

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -8,42 +8,44 @@ class Footer extends Component {
     render(): ReactNode {
         return (
             <div className={s.container}>
-                <div className={s.wrap}>
-                    <div className={s.wrapLogo}>
-                        <BaseIcon name="optimax" />
+                <div className={s.inner}>
+                    <div className={s.wrap}>
+                        <div className={s.wrapLogo}>
+                            <BaseIcon name="optimax" />
+                        </div>
+                        <a className={s.menuLink} href="#">
+                            About Us
+                        </a>
+                        <a className={s.menuLink} href="#">
+                            Location
+                        </a>
+                        <a className={s.menuLink} href="#">
+                            Careers
+                        </a>
+                        <a className={s.menuLink} href="#">
+                            Our Team
+                        </a>
+                        <a className={s.menuLink} href="#">
+                            Blog
+                        </a>
                     </div>
-                    <a className={s.menuLink} href="#">
-                        About Us
-                    </a>
-                    <a className={s.menuLink} href="#">
-                        Location
-                    </a>
-                    <a className={s.menuLink} href="#">
-                        Careers
-                    </a>
-                    <a className={s.menuLink} href="#">
-                        Our Team
-                    </a>
-                    <a className={s.menuLink} href="#">
-                        Blog
-                    </a>
-                </div>
-                <div className={s.socialLinks}>
-                    <a href="#" className={s.socialLink}>
-                        <BaseIcon name="facebook" />
-                    </a>
-                    <a href="#" className={s.socialLink}>
-                        <BaseIcon name="instagram" />
-                    </a>
-                    <a href="#" className={s.socialLink}>
-                        <BaseIcon name="twitter" />
-                    </a>
-                    <a href="#" className={s.socialLink}>
-                        <BaseIcon name="youtube" />
-                    </a>
-                    <a href="#" className={s.socialLink}>
-                        <BaseIcon name="linkdin" />
-                    </a>
+                    <div className={s.socialLinks}>
+                        <a href="#" className={s.socialLink}>
+                            <BaseIcon name="facebook" />
+                        </a>
+                        <a href="#" className={s.socialLink}>
+                            <BaseIcon name="instagram" />
+                        </a>
+                        <a href="#" className={s.socialLink}>
+                            <BaseIcon name="twitter" />
+                        </a>
+                        <a href="#" className={s.socialLink}>
+                            <BaseIcon name="youtube" />
+                        </a>
+                        <a href="#" className={s.socialLink}>
+                            <BaseIcon name="linkdin" />
+                        </a>
+                    </div>
                 </div>
             </div>
         );
