2b9dfbc07419b3f5f9fad3ec03b8c4c703fad5b8
Roman <ya-hostel@yandex.ru>
2019-07-21 17:41:05 +0400

    animation astronaut

--- src/components/home/astronaut/astronaut.tsx
+++ src/components/home/astronaut/astronaut.tsx
@@ -3,7 +3,11 @@ import React from 'react'
 import s from './astronaut.module.scss'
 
 const Astronaut: React.FC = (): React.ReactElement => {
-  return <div className={s.wrap} />
+  return (
+    <div className={s.wrap}>
+      <div className={s.astronaut} />
+    </div>
+  )
 }
 
 export default Astronaut
